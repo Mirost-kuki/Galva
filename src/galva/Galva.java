92350e94998072e62d095579fa63e22e5d9ad848
Pawel Rzeszutko <rzeszutko93@gmail.com>
2019-10-18 14:36:31 +0200

    18.10.2019 14:35 - Window creation

--- src/galva/Galva.java
+++ src/galva/Galva.java
@@ -1,9 +1,30 @@
 package galva;
 
-public class Galva {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Galva extends JFrame
+{
+	private static final long serialVersionUID = 6538101122351606537L;
+
+	public Galva()
+	{
+		setTitle("Galva");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setFocusable(true);
+	}
+	
+	public static void main(String[] args) 
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				new Galva().setVisible(true);
+				
+			}
+		});
 
 	}
 
