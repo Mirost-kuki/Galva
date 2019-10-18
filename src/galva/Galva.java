edc520d8ae889e03cbf2493dfc1c8d7070e09254
Pawel Rzeszutko <rzeszutko93@wp.pl>
2019-10-18 21:35:26 +0200

    18.10.2019 21:35

--- src/galva/Galva.java
+++ src/galva/Galva.java
@@ -1,5 +1,9 @@
 package galva;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -7,14 +11,30 @@ public class Galva extends JFrame
 {
 	private static final long serialVersionUID = 6538101122351606537L;
 
+	Menu menu;
+	
 	public Galva()
 	{
 		setTitle("Galva");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setFocusable(true);
+		
+		menu = new Menu();
+		
+		WindowLayout();
+		pack();
 	}
 	
+	private void WindowLayout()
+	{
+		this.getRootPane().setBorder(BorderFactory.createMatteBorder(8, 8, 8, 8, Color.GRAY));
+		this.getContentPane().setLayout(new BorderLayout(8, 6));
+		
+		setJMenuBar(menu);
+		
+	}
+
 	public static void main(String[] args) 
 	{
 		SwingUtilities.invokeLater(new Runnable() {
